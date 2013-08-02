57964db8650e3e65473ab0c2099ae407997101fa
Prakhar Singh <prakharsingh95@gmail.com>
2013-08-02 17:50:40 +0530

    Added new functions for new player, exit game

---
+++ server/server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var http = require('http')
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+// All routing goes here.
+app.get('/', function (request, response) 
+{
+	res.sendfile(__dirname + '/index.html');
+});
+
+//Constants
+const G_MAX_PLAYERS_PER_GAME;
+//Runtime Variables
+
+var ggames = {};	//Active Games
+var gplayers = {}; //Global Player List
+
+io.sockets.on('connection', function (socket){
+	socket.on('newPlayer', function(userName, game){
+		socket.userName = userName;
+		if(ggames['game'].players<G_MAX_PLAYERS_PER_GAME)
+		{
+			socket.game = game;
+			ggames['game'].players++;	
+			gplayers['userName'] = userName;
+			socket.join(ggames['game']);
+			socket.emit('newPlayerSuccess', 'Connected to game: '+game);
+		}	
+		else
+			socket.emit('newPlayerError', 'This game is full');
+		console.log('New player ' + socket.userName +' has connected to game: ' + game);	
+	});
+
+	socket.on('exitGame', function(){
+		console.log('Player ' + socket.userName +' has left the game: ' + game);
+		socket.leave(socket.game);
+		if(ggames['game'].players<=1)
+		{
+			delete ggames[socket.game];
+			console.log('Game: ' + game + 'has been destroyed');
+		}
+		else
+			socket.broadcast.to(socket.room).emit('playerExited', socket.userName, ggames[socket.game].players);
+		socket.emit('exitGameSuccess', 'Exited from game: '+socket.game);
+		socket.game = "";
+	});
+});
