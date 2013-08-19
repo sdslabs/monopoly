5c6fe2c9a85169901c37f71288c591dbaa72f93a
Prakhar Singh <prakharsingh95@gmail.com>
2013-08-19 20:12:08 +0530

    Completed client authorization (without API)

--- server/authorize.js
+++ server/authorize.js
@@ -3,21 +3,49 @@ var CONST = require('./constants.js');
 
 //Runtime Variables
 var games = {};	//Active Games
-var gplayers = {}; //Global Player List
+var gPlayers = {}; //Global Player List
+
+// function newGame(){
+// 	this.creator = null;
+// 	this.createdAt = null;
+// 	this.
+// }
+
+// function globalAuth(){
+// 	for(var key in gPlayers){
+// 		if(p.hasOwnProperty(key)) {
+
+// 		}
+// 	}
+// }
+
+//Protoypes of functions affecting gPlayers ===== START =====
+function Player(){
+	this.lastActivity = null;
+	this.currentGame = null;
+	this.sessionID = null;
+}
+
+Player.prototype.isAuth = function(socket){
+	if(this.sessionID == socket.handshake.sessionID)
+		return true;
+	else
+		return false;
+}
+//Protoypes of functions affecting gPlayers =====END==========
+
 
 function initialize(io, express){
 	games['game'] = {};
 	games['game'].players = 0;
 
 	//Authenication Needed
-
 	io.set('authorization', function (data, accept) {
    		// check if there's a cookie header
- 
     	if (data.headers.cookie) {
         	// if there is, parse the cookie
         	data.cookie = express.cookieParser(data.headers.cookie);
-        	// retri
+        	// retrive the cookie
         	data.sessionID = data.cookie['connect.sid'];
     	} else {
        	// if there isn't, turn down the connection with a message
@@ -30,88 +58,67 @@ function initialize(io, express){
 
 	io.sockets.on('connection', function (socket){
 
-		socket.on('addNewPlayer', function(userName){
-			socket.userName = userName;
+		socket.on('addNewPlayer', function(playerName){
+			socket.playerName = playerName;
 		
-			gplayers[userName] = {};
-			gplayers[userName].lastActivity = new Date();
-			gplayers[userName].currentGame = '';
-			gplayers[userName].isAuth = true;
+			gPlayers[playerName] = new Player();
+			gPlayers[playerName].lastActivity = new Date();
+			gPlayers[playerName].currentGame = '';
+			gPlayers[playerName].sessionID = socket.handshake.sessionID;
 
-			socket.emit('addNewPlayerSuccess', userName);
+			socket.isAuth = true;
+			socket.emit('addNewPlayerSuccess', playerName);
+			console.log('[' + new Date()+ '] '+ socket.playerName + ' logged in.');
+
 		});
+		
+	socket.on('createGame', function(game){
+			socket.game = game;
 
-		socket.on('createGame', function(game){
-			if(gplayers[socket.userName].isAuth == false){
-				socket.emit('createGameError', 'Unauthorized access');
-				console.log('Illegal request to create '+ game + ' by '+socket.userName);
-			}
-			else
-			{
-				socket.game = game;
-
-				games[game] = {};
-				games[game].players+=1;
-				
-				gplayers[socket.userName].currentGame = 'game';
-				
-				//gplayers[userName] = userName;
-				socket.join(game);
-				socket.emit('addToGameSuccess', 'Connected to game: '+game);
-				socket.broadcast.to(game).emit('newPlayerAdded', socket.userName);
-				console.log('New player ' + socket.userName +' has connected to game: ' + game);
-			}
+			games[game] = {};
+			games[game].players+=1;
+			
+			gPlayers[socket.playerName].currentGame = 'game';
+			//gPlayers[playerName] = playerName;
+			socket.join(game);
+			socket.emit('addToGameSuccess', 'Connected to game: '+game);
+			socket.broadcast.to(game).emit('newPlayerAdded', socket.playerName);
+			console.log('[' + new Date()+ '] '+ socket.playerName +' connected to game: ' + game);
 		});
 
 		socket.on('addToGame', function(game){
-			if(gplayers[socket.userName].isAuth == false){
-				socket.emit('createGameError', 'Unauthorized access');
-			console.log('Illegal request to create '+ game + ' by '+socket.userName);
-			}
-			else if(games[game].players<CONST.G_MAX_PLAYERS_PER_GAME)
+			if(games[game].players<CONST.G_MAX_PLAYERS_PER_GAME)
 			{
 				socket.game = game;
 				games[game].players+=1;	
 				socket.join(game);
 				socket.emit('addToGameSuccess', 'Connected to game: '+game);
-				socket.broadcast.to(game).emit('newPlayerAdded', socket.userName, games[game].players);
-				console.log('New player ' + socket.userName +' has connected to game: ' + game);
+				socket.broadcast.to(game).emit('newPlayerAdded', socket.playerName, games[game].players);
+				console.log('[' + new Date()+ '] '+ 'Player ' + socket.playerName +' has connected to game: ' + game);
 			}	
 			else{	
 				socket.emit('addToGameError', 'This game is full!!!');
-				console.log(games[game] + ' '+ games[game].players);
+				console.log('[' + new Date()+ '] '+ 'Player' + socket.playerName + ' refused access to '+ game + '.This game is full.');
 			}
 		});
 
-		socket.on('queryPlayerList', function(){
-			if(gplayers[socket.userName].isAuth == false){
-				socket.emit('createGameError', 'Unauthorized access');
-				console.log('Illegal request to create '+ game + ' by '+socket.userName);
-			}
-			else
-				socket.emit('updateUserList', JSON.stringify(gplayers));
-		});
+		// socket.on('queryPlayerList', function(){
+		// 	socket.emit('updateUserList', JSON.stringify(gPlayers));
+		// });
 		
 		socket.on('exitFromGame', function(){
-			if(gplayers[socket.userName].isAuth == false){
-				socket.emit('createGameError', 'Unauthorized access');
-				console.log('Illegal request to create '+ game + ' by '+socket.userName);
-			}
-			else{
-				games[socket.game].players -= 1;
-				socket.leave(socket.game);
-				console.log(socket.userName +' has left the game: ' + socket.game);
+			games[socket.game].players -= 1;
+			socket.leave(socket.game);
+			console.log('[' + new Date()+ '] '+ socket.playerName +' has left the game: ' + socket.game);
 				
-				if(games['game'].players<1)
-				{
-					delete games[socket.game];
-					console.log('Game: ' + game + 'has been destroyed');
-				}
-				else{	
-					socket.broadcast.to(socket.game).emit('playerExited', socket.userName);
-					socket.emit('exitFromGameSuccess', 'socket.game');
-					socket.game = "";
-				}
+			if(games['game'].players<1){
+				delete games[socket.game];
+				console.log('[' + new Date()+ '] '+ 'Game: ' + game + 'has been destroyed');
+			}
+			else{	
+				socket.broadcast.to(socket.game).emit('playerExited', socket.playerName);
+				socket.emit('exitFromGameSuccess', 'socket.game');
+				socket.game = "";
 			}
 		});	
 	});
