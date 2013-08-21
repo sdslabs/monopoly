cb990538b94013c1103754799f1b5f5649ca9ba7
Prakhar Singh <prakharsingh95@gmail.com>
2013-08-21 20:00:36 +0530

    Created basic mp files

---
+++ server/objects.js
@@ -0,0 +1,16 @@
+function Player(){
+	this.playerName = null;
+	this.lastActivity = null;
+	this.currentGame = null;
+	this.sessionID = null;
+}
+
+function Game(){
+	this.creator = null;
+	this.createdAt = null;
+	this.players = {};
+	this.totalPlayers = 0;
+}
+
+module.exports.Game = Game;
+module.exports.Player = Player
