121cbdda2f9a2fab0fa123ed060e14198057dca1
Prakhar Singh <prakharsingh95@gmail.com>
2013-08-26 18:18:19 +0530

    Removed readme (already in redmine). Added logging to router.

--- router.js
+++ router.js
@@ -1,7 +1,17 @@
+//Load the Global Function Module
+var global = require('./global.js');
+
 function initialize (app){
 	app.get('/', function (req, res) {
 		res.sendfile(__dirname + '/public/index.html');
+		global.log('info', 'Sent file: /public/index.html to client: ' + req.connection.remoteAddress);
 	});
+
+	app.get('/rootCA', function(req, res){
+		res.setHeader('Content-Type', 'application/x-pem-file');
+		res.sendfile(__dirname + '/ssl/root/rootCA.pem');
+		global.log('info', 'Sent X.509 root CA certificate: to client: ' + req.connection.remoteAddress);
+	})
 
 	// app.get('/favicon.ico', function(req, res){
 	// 	res.sendfile(__dirname + '/favicon.ico');
