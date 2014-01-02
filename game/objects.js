2fa4b45ec6085e49e6eb35693096d2825f7a68a6
Prakhar Singh <prakharsingh95@gmail.com>
2014-01-02 19:40:40 +0530

    Combined paths and properties json files

--- game/objects.js
+++ game/objects.js
@@ -125,56 +125,34 @@ Game.prototype.morePlayersAllowed = function(){
 	return this.totalPlayers <= M_CONST.MAX_PLAYERS_PER_GAME;
 }
 
-//Load Constants
-var CONST = require('../constants.js');
-
 //Load Properties
 var properties = require('./properties.json');
 
 //Load Paths
 var paths = require('./paths.json');
 
-function Location(){
-	this.x = 0;
-	this.y = 0;
-	this.z = 0;
-}
+// OBJECT DESCRIPTION 
 
-function Size(){
-	this.width = 0;
-	this.height = 0;
-}
+// function Location(){
+// 	this.x = 0;
+// 	this.y = 0;
+// 	this.z = 0;
+// }
 
-function Level(){
-	var rent;
-	var upgrade;
-}
+// function Property(){
+// 	this. id = null;
+// 	this.name = null;
+// 	this.owner = null;
+// 	this.type = null;
+// 	this.maxLevel = null;
+// 	this.basePrice = 0;
+// 	this.Location = new Location();
+// }
 
-function Property(){
-
-	this. id = null;
-
-	this.name = null;
-	this.owner = null;
-	this.type = null;
-
-	this.level = null;
-	this.maxLevel = null;
-	
-	this.basePrice = 0;
-
-	this.Size = new Size();
-	this.Location = new Location();
-
-}
-
-function Map(){
-	this.properties = properties;
-	this.paths = paths;
-
-	this.createdAt = null;
-	this.type = 'monopoly';
-}
+// function Map(){
+// 	this.properties = properties;
+// 	this.paths = paths;
+// }
 
 module.exports.Map = Map;
 module.exports.Game = Game;
