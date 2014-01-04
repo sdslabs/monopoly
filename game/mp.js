089c1c22a49fed73336536df0608d4e69d2eae39
Prakhar Singh <prakharsingh95@gmail.com>
2014-01-04 21:14:26 +0530

    Added and debugged multiple functions to mp.js (also added constants in json file)

--- game/mp.js
+++ game/mp.js
@@ -1,15 +1,17 @@
 //Load Game Specific Constants
 var M_CONST = require('./m_constants.json');
 
+//Load the Global Function Module
+var global = require('../global.js');
+
 var Games = null;
 var Players = null;
 var game = null;
 
-function mp(map, game, socket){
+function mp(game, socket){
 	// this.startedAt = new Date();
 	this.game = game;
 	this.socket = socket;
-	this.map = Map;
 	this.currentPlayer = 0;
 }
 
@@ -62,33 +64,52 @@ function init(G_ames, P_layers, socket){
 	socket.on('mpInitialize', function(){
 			var player = findPlayer(socket);
 			player.money = M_CONST.INITIAL_AMOUNT;
-			player.location.x = player.location.y = M_CONST.START_COORD;
+			player.locProp = M_CONST.START_PROP;
+			console.log(Players);
+			socket.emit("mpInitComplete");
 
 	});
 
-	socket.on('mpMove', function(moves, path){
+	socket.on('mpMove', function(moves, route){
 		var game = findGame(socket);
+		var player = findPlayer(socket);
 		var flag = false;
+
 		if(game){
-			for(var i = 0; i<moves-1; i++)
-				if(!game.map.properties[path[i]].paths.hasOwnProperty(path[i+1])){
-					flag = true;
-					break;
+			if(moves <= 6){
+				var i = 0;
+				if(player.locProp == M_CONST.START_PROP){
+					player.locProp = M_CONST.FIRST_PROP;
+					i++;
 				}
 
+				for(; i<moves; i++){
+					if(!game.map.doesPathExist(player.locProp, route[i])){
+						global.log('warn', "Illegal move by " + socket.playerName + " in game " + player.getCurrentGame() + ". No path between " + player.locProp + ", " + route[i]);
+						flag = true;
+						break;
+					}
+					player.locProp = route[i];
+				}
+			}else
+				flag = true;
+				
 			if(!flag){
-				var nextPlayer = game.mp.getNextPlayer();
-
-				socket.broadcast.to(game).emit('mpEnemyPath', path, nextPlayer);
-				socket.emit("mpMoveAccepted", nextPlayer);
+				// var nextPlayer = game.mp.getNextPlayer();
+				// socket.broadcast.to(game).emit('mpPlayerMove', route, nextPlayer);
+				// socket.emit("mpMoveAccepted", nextPlayer);
+				global.log('verbose', "Move by " + socket.playerName + " in game " + player.getCurrentGame() + ". Route " + route + ".");
 			}
-			else
+			else{
 				socket.emit("mpMoveRejected");
+			}
 		}
 	});
 
-	socket.on('PING2', function(){
+	socket.on('PING2', function(garb1, garb2){
+		
 		console.log('PING2 RECEIVED');
+		console.log(findPlayer(socket));
 	});
 }
 
