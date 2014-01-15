9e6dbc92d888af6f09b0a580a784c904ed699a8c
Prakhar Singh <prakharsingh95@gmail.com>
2014-01-15 21:49:17 +0530

    Added basic marker functions

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,16 +1,37 @@
-var map = (function(){
+var graphics = (function(){
+
+	var map;
 
 	function initialize() {
 		var mapOptions = {
 			zoom: 15,
 			center: new google.maps.LatLng(29.86535, 77.89475)
 		};
-		var M = new google.maps.Map(document.getElementById('map-canvas'),
+		map = new google.maps.Map(document.getElementById('map-canvas'),
 							mapOptions);
-		console.log("here");
+		return M;
 	}
 
+	function setMarkerCenter() {
+		  var marker = new google.maps.Marker({
+			position: map.getCenter(),
+			map: map,
+			title: 'Click to zoom'
+  		});
+	}
+
+	function setMarkerAt(lat, lng, title) {
+		var pos = new google.maps.LatLng(lat, lng);
+		var marker = new google.maps.Marker({
+			position: pos,
+			map: map,
+			title: title
+  		});
+	}
 	return {
-		init:initialize
+		init:initialize,
+		setMarkerCenter:setMarkerCenter,
+		setMarkerAt:setMarkerAt
+
 	}
 })();
