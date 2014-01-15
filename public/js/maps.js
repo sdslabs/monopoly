58ea49a28b1970136d2e00ca2decb4cadeeaaa1d
Prakhar Singh <prakharsingh95@gmail.com>
2014-01-15 22:26:51 +0530

    Added more map functions for markers and listeners

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,10 +1,10 @@
-var graphics = (function(){
+var G = (function(){
 
 	var map;
 
 	function initialize() {
 		var mapOptions = {
-			zoom: 15,
+			zoom: 4,
 			center: new google.maps.LatLng(29.86535, 77.89475)
 		};
 		map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -12,7 +12,7 @@ var graphics = (function(){
 		return M;
 	}
 
-	function setMarkerCenter() {
+	function addMarkerCenter() {
 		  var marker = new google.maps.Marker({
 			position: map.getCenter(),
 			map: map,
@@ -20,18 +20,42 @@ var graphics = (function(){
   		});
 	}
 
-	function setMarkerAt(lat, lng, title) {
-		var pos = new google.maps.LatLng(lat, lng);
+	function addMarkerAt(latlng, cap) {
 		var marker = new google.maps.Marker({
-			position: pos,
+			position: latlng,
 			map: map,
-			title: title
+			title: cap
   		});
 	}
+
+	function addListener(type, action) {
+		 google.maps.event.addListener(map, type, action);
+	}
+
+	function getLocation(lat, lng){
+		return new google.maps.LatLng(lat, lng);
+	}
+
+	function getMap(){
+		return map;
+	}
+
 	return {
+		M:getMap,
 		init:initialize,
-		setMarkerCenter:setMarkerCenter,
-		setMarkerAt:setMarkerAt
+		addMarkerCenter:addMarkerCenter,
+		addMarkerAt:addMarkerAt,
+		addListener:addListener,
+		getLocation:getLocation
 
 	}
 })();
+
+
+// Testing functions
+function clickListener() {
+	G.addListener('click', function(event){
+		G.addMarkerAt(event.latLng, prompt("Marker Tag?"));
+	});
+
+}
