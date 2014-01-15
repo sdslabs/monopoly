936c0babd12987d4d5da5724ca40c92c5f5cf71e
jayant1992 <jayant_jain1992@yahoo.co.in>
2014-01-15 23:01:47 +0530

    Added bounds checking for google map

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,12 +1,42 @@
 var maps = (function()
 {
+	var gmap;
 	var init = function() 
 	{
+		google.maps.visualRefresh = true;
 		var mapOptions = {
 			zoom: 15,
 			center: new google.maps.LatLng(29.86535, 77.89475)
 		};
-		var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+		gmap = new google.maps.Map($('#map-canvas')[0], mapOptions);
+		defineBound()
+		logStats()
+	}
+
+	var defineBound = function()
+	{
+		var allowedBounds = new google.maps.LatLngBounds(
+		    new google.maps.LatLng(29.85986892392962, 77.88862466812134),
+			new google.maps.LatLng(29.87304327830209, 77.90328025817871)
+		);
+
+		var lastValidCenter = gmap.getCenter();
+		google.maps.event.addListener(gmap, 'center_changed', function() {
+			console.log(gmap.getCenter())
+		    if (allowedBounds.contains(gmap.getCenter())) {
+		        lastValidCenter = gmap.getCenter();
+		        return; 
+		    }
+		    gmap.panTo(lastValidCenter);
+		});
+	}
+
+	var logStats = function()
+	{
+		google.maps.event.addListener(gmap, 'click', function( event ){
+			console.log( "Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng() ); 
+			console.log(gmap.getZoom())
+		});
 	}
 
 	return {
