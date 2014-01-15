6b1dde037654b71c17d14ded9502694b7293da5f
Prakhar Singh <prakharsingh95@gmail.com>
2014-01-15 22:54:49 +0530

    Added marker icon and window functions

--- public/js/maps.js
+++ public/js/maps.js
@@ -18,19 +18,56 @@ var G = (function(){
 			map: map,
 			title: 'Click to zoom'
   		});
+		return marker;
 	}
 
-	function addMarkerAt(latlng, cap) {
+	function addMarkerAt(latLng, cap) {
 		var marker = new google.maps.Marker({
-			position: latlng,
+			position: latLng,
 			map: map,
 			title: cap
   		});
+  		return marker;
+	}
+
+	function addMarkerImage(latLng, image_url, shadow_url, cap){
+		var image = new google.maps.MarkerImage(image_url,
+      					new google.maps.Size(20, 32),
+      					new google.maps.Point(0,0),
+     					new google.maps.Point(0, 32));
+		var shadow = null;
+		if(shadow_url)
+			shadow = new google.maps.MarkerImage(shadow_url,
+						new google.maps.Size(37, 32),
+      					new google.maps.Point(0,0),
+      					new google.maps.Point(0, 32));
+		var marker = new google.maps.Marker({
+			position: latLng,
+			map: map,
+			icon:image,
+			shadow:shadow,
+			title: cap
+  		});
+
+  		return marker;
+	}
+
+	function addInfoWindow(marker, cap) {
+		var infowindow = new google.maps.InfoWindow({
+			content: cap,
+			size: new google.maps.Size(50,50)
+		});
+		infowindow.open(map, marker);
+		return infowindow;
 	}
 
 	function addListener(type, action) {
 		 google.maps.event.addListener(map, type, action);
 	}
+
+	function addListenerOth(obj, type, action) {
+		 google.maps.event.addListener(obj, type, action);
+	}	
 
 	function getLocation(lat, lng){
 		return new google.maps.LatLng(lat, lng);
@@ -45,7 +82,10 @@ var G = (function(){
 		init:initialize,
 		addMarkerCenter:addMarkerCenter,
 		addMarkerAt:addMarkerAt,
+		addMarkerImage:addMarkerImage,
 		addListener:addListener,
+		addListenerOth:addListenerOth,
+		addInfoWindow:addInfoWindow,
 		getLocation:getLocation
 
 	}
@@ -55,7 +95,8 @@ var G = (function(){
 // Testing functions
 function clickListener() {
 	G.addListener('click', function(event){
-		G.addMarkerAt(event.latLng, prompt("Marker Tag?"));
+		var m = G.addMarkerAt(G.M().getCenter(), "click");
+		G.addListenerOth(m, 'click', function(){G.addInfoWindow(m, prompt("Caption?"))});
 	});
 
 }
