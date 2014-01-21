2d117f789e640d89e7d4ea91dce46b26e3f39c32
jayant1992 <jayant_jain1992@yahoo.co.in>
2014-01-21 21:54:52 +0530

    1. Updates paths for js files in leaderboard template 2. Fixes minor bug in leaderboard.js 3. Shifts global function in leaderboard.js

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,27 +1,30 @@
 var start = 0; var loaded = false;
 
 $(document).ready(function(){
+
+	function update(start) {
+		$.post('/leaderboard', {start:start}, function(data){
+			var obj = JSON.parse(data);
+			if(obj.hasOwnProperty('results'))
+				if(!obj.results.length){
+					loaded = true;
+					$('#loading-alert').hide();
+					$('#loaded-alert').show();
+				}
+				else{
+					$('#score-table-template').tmpl(obj.results).appendTo('#score-table tbody');
+					$('#loaded-alert').hide();
+					$('#loading-alert').show();
+				}
+		});
+	}
+
 	if(typeof topbar != 'undefined' && topbar != null){
 		topbar.showTopbar();
 	}
 	update();
 });
 
-function update(start) {
-	$.post('/leaderboard', {start:start}, function(data){
-		var obj = JSON.parse(data);
-		if(!obj.results.length){
-			loaded = true;
-			$('#loading-alert').hide();
-			$('#loaded-alert').show();
-		}
-		else{
-			$('#score-table-template').tmpl(obj.results).appendTo('#score-table tbody');
-			$('#loaded-alert').hide();
-			$('#loading-alert').show();
-		}
-	});
-}
 
 
 $(window).scroll(function() { 
